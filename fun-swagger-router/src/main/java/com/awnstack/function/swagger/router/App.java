744b25f759856af35b6355826494308371faa395
Ares <yangzibin.cn@hotmail.com>
2017-04-14 19:08:56 +0800

    init

---
+++ fun-swagger-router/src/main/java/com/awnstack/function/swagger/router/App.java
@@ -0,0 +1,13 @@
+package com.awnstack.function.swagger.router;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
